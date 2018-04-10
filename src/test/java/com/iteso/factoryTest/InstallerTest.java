f68f61b2f046ec13707137babc743736b2e9f739
Mariana <mari.anasieveg2@gmail.com>
2018-04-09 19:57:38 -0500

    Unit Test

--- src/test/java/com/iteso/factoryTest/InstallerTest.java
+++ src/test/java/com/iteso/factoryTest/InstallerTest.java
@@ -1,5 +1,9 @@
 package com.iteso.factoryTest;
 
+import com.iteso.factory.DownloadFactory;
+import com.iteso.factory.Factory.LinuxFactory;
+import com.iteso.factory.Factory.MacOSFactory;
+import com.iteso.factory.Factory.WindowsFactory;
 import com.iteso.factory.Installer;
 import com.iteso.factory.InstallerPackage;
 import com.iteso.factory.installerPackages.LinuxInstallerPackage;
@@ -13,6 +17,7 @@ import org.junit.Before;
 public class InstallerTest {
     Installer installer;
     InstallerPackage installerPackage;
+    DownloadFactory downloadFactory;
     @Before
     public void setUp(){
     }
@@ -35,7 +40,26 @@ public class InstallerTest {
         Assert.assertTrue(installer.getPackage() instanceof WindowsInstallerPackage);
         installer = new WindowsInstallerProfessional();
         Assert.assertTrue(installer.getPackage() instanceof WindowsInstallerPackage);
+
     }
+
+    @Test
+    public void WindowsInstallerPackageFuncTest(){
+        installer = new WindowsInstallerFree();
+        Assert.assertEquals(installer.getPackage().decode(), "decoding Windows");
+        Assert.assertEquals(installer.getPackage().execute(), "executing Windows");
+        Assert.assertEquals(installer.getPackage().encode(), "encode Windows");
+        Assert.assertEquals(installer.getPackage().extract(), "extract Windows");
+    }
+
+    @Test
+    public void WindowsFactoryTest(){
+        downloadFactory = new WindowsFactory();
+        Assert.assertTrue(downloadFactory.downloadInstaller("Free") instanceof WindowsInstallerFree);
+        Assert.assertTrue(downloadFactory.downloadInstaller("Home") instanceof WindowsInstallerHome);
+        Assert.assertTrue(downloadFactory.downloadInstaller("Professional") instanceof WindowsInstallerProfessional);
+    }
+
 
     @Test
     public void MacOSInstallerTest(){
@@ -58,6 +82,23 @@ public class InstallerTest {
     }
 
     @Test
+    public void MacOSFactoryTest(){
+        downloadFactory = new MacOSFactory();
+        Assert.assertTrue(downloadFactory.downloadInstaller("Free") instanceof MacOSInstallerFree);
+        Assert.assertTrue(downloadFactory.downloadInstaller("Home") instanceof MacOSInstallerHome);
+        Assert.assertTrue(downloadFactory.downloadInstaller("Professional") instanceof MacOSInstallerProfessional);
+    }
+
+    @Test
+    public void MacOSInstallerPackageFuncTest(){
+        installer = new MacOSInstallerFree();
+        Assert.assertEquals(installer.getPackage().decode(), "decoding MacOS");
+        Assert.assertEquals(installer.getPackage().execute(), "executing MacOS");
+        Assert.assertEquals(installer.getPackage().encode(), "encode MacOS");
+        Assert.assertEquals(installer.getPackage().extract(), "extract MacOS");
+    }
+
+    @Test
     public void LinuxInstallerTest(){
         installer = new LinuxInstallerUbuntu();
         Assert.assertEquals(installer.getName(), "Linux Installer Ubuntu");
@@ -75,5 +116,22 @@ public class InstallerTest {
         Assert.assertTrue(installer.getPackage() instanceof LinuxInstallerPackage);
         installer = new LinuxInstallerUbuntu();
         Assert.assertTrue(installer.getPackage() instanceof LinuxInstallerPackage);
+    }
+
+    @Test
+    public void LinuxFactoryTest(){
+        downloadFactory = new LinuxFactory();
+        Assert.assertTrue(downloadFactory.downloadInstaller("Ubuntu") instanceof LinuxInstallerUbuntu);
+        Assert.assertTrue(downloadFactory.downloadInstaller("Elementary") instanceof LinuxInstallerElementary);
+        Assert.assertTrue(downloadFactory.downloadInstaller("Fedora") instanceof LinuxInstallerFedora);
+    }
+
+    @Test
+    public void LinuxInstallerPackageFuncTest(){
+        installer = new LinuxInstallerUbuntu();
+        Assert.assertEquals(installer.getPackage().decode(), "decoding Linux");
+        Assert.assertEquals(installer.getPackage().execute(), "executing Linux");
+        Assert.assertEquals(installer.getPackage().encode(), "encode Linux");
+        Assert.assertEquals(installer.getPackage().extract(), "extract Linux");
     }
 }
