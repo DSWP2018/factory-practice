74087bb87cc88ad7e49daed91e69341be5e05ba5
JulianLz96 <jj_lz96@hotmail.com>
2018-04-09 19:40:01 -0500

    test getPackage

--- src/Test/java/InstallerTestTest.java
+++ src/Test/java/InstallerTestTest.java
@@ -1,4 +1,8 @@
 import com.iteso.factory.Installer;
+import com.iteso.factory.Package.LinuxPackage;
+import com.iteso.factory.Package.MacOSPackage;
+import com.iteso.factory.Package.WindowsPackage;
+import com.iteso.factory.Store.MacOS;
 import com.iteso.factory.installers.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,30 +21,39 @@ public class InstallerTestTest {
         installer = new FreeLinux();
         assertEquals("LinuxFree", installer.getName());
         assertEquals("executeando Linux", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof LinuxPackage);
         installer = new HomeLinux();
         assertEquals("HomeLinux", installer.getName());
         assertEquals("executeando Linux", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof LinuxPackage);
         installer = new ProfessionalLinux();
         assertEquals("ProfessionalLinux", installer.getName());
         assertEquals("executeando Linux", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof LinuxPackage);
         installer = new FreeWindows();
         assertEquals("FreeWindows", installer.getName());
         assertEquals("executeando Windows", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof WindowsPackage);
         installer = new HomeWindows();
         assertEquals("HomeWindows", installer.getName());
         assertEquals("executeando Windows", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof WindowsPackage);
         installer = new ProfessionalWindows();
         assertEquals("ProfessionalWindows", installer.getName());
         assertEquals("executeando Windows", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof WindowsPackage);
         installer = new FreeMacOS();
         assertEquals("FreeMacOS", installer.getName());
         assertEquals("executeando MacOS", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof MacOSPackage);
         installer = new HomeMacOS();
         assertEquals("HomeMacOS", installer.getName());
         assertEquals("executeando MacOS", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof MacOSPackage);
         installer = new ProfessionalMacOS();
         assertEquals("ProfessionalMacOS", installer.getName());
         assertEquals("executeando MacOS", installer.getPackage().execute());
+        assertTrue(installer.getPackage() instanceof MacOSPackage);
     }
 
 }
